596ef3eeecbf208465b8efd3589fc5a46bb10996
YadaDelta <dyashin6900@mail.ru>
2023-02-11 10:02:51 +0400

    Refactor code

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,18 +8,22 @@ const stylishResult = readFileSync('__fixtures__/expected.stylish.txt', 'utf-8')
 const plainResult = readFileSync('__fixtures__/expected.plain.txt', 'utf-8');
 const jsonResult = readFileSync('__fixtures__/expected.json.txt', 'utf-8');
 
-const formatList = ['json', 'yaml', 'yml'];
-
-test.each(formatList)('testing stylish nested', (format) => {
-  expect(calcDiff(`__fixtures__/file1.${format}, __fixtures__/file2.${format}`)).toBe(stylishResult);
+test('testing stylish nested', () => {
+  expect(calcDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(stylishResult);
+  expect(calcDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml')).toBe(stylishResult);
+  expect(calcDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml')).toBe(stylishResult);
 });
 
-test.each(formatList)('testing plain nested', (format) => {
-  expect(calcDiff(`__fixtures__/file1.${format}, __fixtures__/file2.${format}`)).toBe(plainResult);
+test('testing plain nested', () => {
+  expect(calcDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain')).toBe(plainResult);
+  expect(calcDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'plain')).toBe(plainResult);
+  expect(calcDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'plain')).toBe(plainResult);
 });
 
-test.each(formatList)('testing json nested', (format) => {
-  expect(calcDiff(`__fixtures__/file1.${format}, __fixtures__/file2.${format}`)).toBe(jsonResult);
+test('testing json nested', () => {
+  expect(calcDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'json')).toBe(jsonResult);
+  expect(calcDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'json')).toBe(jsonResult);
+  expect(calcDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'json')).toBe(jsonResult);
 });
 
 test('should be errors', () => {
